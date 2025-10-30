3700233c4ef75a4fd0504cf9ff41f59eb5e03c4b
Kalen Young <kalenyoung03@gmail.com>
2025-10-30 20:14:26 +0200

    chore: switch from Node adapter to Vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   server: {
     port: 4321
   }
